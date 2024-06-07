331dd7cbfff1c27854cdfd74d680763bd382fd61
kaantrz <93608073+kaantrz@users.noreply.github.com>
2024-06-07 14:34:52 +0300

    Create NotificationRecord.java

---
+++ src/NotificationRecord.java
@@ -0,0 +1,25 @@
+package com.yourcompany.notificationservice;
+
+public class NotificationRecord {
+    private String msisdn;
+    private int packageId;
+    private int notificationType;
+
+    public NotificationRecord(String msisdn, int packageId, int notificationType) {
+        this.msisdn = msisdn;
+        this.packageId = packageId;
+        this.notificationType = notificationType;
+    }
+
+    public String getMsisdn() {
+        return msisdn;
+    }
+
+    public int getPackageId() {
+        return packageId;
+    }
+
+    public int getNotificationType() {
+        return notificationType;
+    }
+}
